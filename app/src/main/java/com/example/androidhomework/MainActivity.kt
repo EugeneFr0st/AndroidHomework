e14fea13f1ca15c29f8fac213a385fe7e93bbc4e
Eugene <emoroz321@gmail.com>
2024-11-09 18:05:49 +0300

    copleted homework_16

--- app/src/main/java/com/example/androidhomework/MainActivity.kt
+++ app/src/main/java/com/example/androidhomework/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.androidhomework
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val viewNotesButton = findViewById<Button>(R.id.viewNotesButton)
+        val addNoteButton = findViewById<Button>(R.id.addNoteButton)
+
+        viewNotesButton.setOnClickListener {
+            val intent = Intent(this, OnboardingActivity1::class.java)
+            startActivity(intent)
+        }
+
+        addNoteButton.setOnClickListener {
+            val intent = Intent(this, OnboardingActivity3::class.java)
+            startActivity(intent)
         }
     }
 }
